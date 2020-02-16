b50960993c41ba9213040d1a1bde936121991f6f
Tamer UZUN <yigitruzgaruzun@gmail.com>
2020-02-16 17:14:40 +0300

    iki

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("tamer");
+const http=require("http")
+
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('Merhaba Tamer');
+    res.end();
+  }).listen(8080);
+
+
+
