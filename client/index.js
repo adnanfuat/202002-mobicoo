dff9572aa0e26a95b5f2fe2ccf55a0150befd867
Tamer UZUN <yigitruzgaruzun@gmail.com>
2020-02-26 23:47:43 +0300

    toplu3

--- client/index.js
+++ client/index.js
@@ -3,7 +3,7 @@ const http=require("http")
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Merhaba Tamer');
+    res.write('Merhaba Tamerim');
     res.end();
   }).listen(8080);
 
