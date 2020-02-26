afac2d1bf3f8f9206206f2d5a8ba3de02539b9fc
Tamer UZUN <yigitruzgaruzun@gmail.com>
2020-02-26 23:56:33 +0300

    dört

--- client/index.js
+++ client/index.js
@@ -1,9 +1,12 @@
 const http=require("http")
 
+const test = () => console.log("başardım")
+
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.write('Merhaba Tamerim');
+    test();
     res.end();
   }).listen(8080);
 
